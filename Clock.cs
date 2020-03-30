0c4c5e9be016edcbdbf734e0200440d1ec194c6e
JohnEnevoldsen <62882814+JohnEnevoldsen@users.noreply.github.com>
2020-03-30 13:36:36 +0200

    Initial commit

---
+++ Clock.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ClockV10
+{
+    public class Clock
+    {
+        private int _timeInMinutesSince0000;
+
+        public int CurrentTimeInMinutes{
+            get{return _timeInMinutesSince0000%60;}
+        }
+
+        public int CurrentTimeInHours{
+            get{return _timeInMinutesSince0000%1440/60;}
+        }
+        public void SetTime(int hours, int minutes){
+            _timeInMinutesSince0000 = 60 *hours + minutes;
+        }
+        public void DisplayTime(){
+            Console.WriteLine($"The clock is pointing at {CurrentTimeInHours:D2}:{CurrentTimeInMinutes:D2}");
+        }
+
+        public void AdvanceTimeByOneMinute(){
+            _timeInMinutesSince0000++;
+        }
+    }
+}
---
+++ InsertCodeHere.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ClockV10
+{
+    public class InsertCodeHere
+    {
+        public void MyCode()
+        {
+            // The FIRST line of code should be BELOW this line
+
+            Clock myBigWhiteClock = new Clock();
+            myBigWhiteClock.DisplayTime();
+            myBigWhiteClock.SetTime(23, 59);
+            myBigWhiteClock.DisplayTime();
+            myBigWhiteClock.AdvanceTimeByOneMinute();
+            myBigWhiteClock.DisplayTime();
+            // The LAST line of code should be ABOVE this line
+        }
+    }
+}
